6b52e002a57ee035db900e70efa1f822525e1d65
kemenes_janos <kemenes.janos1999@gmail.com>
2019-10-01 17:09:04 +0200

    Added VS solution

---
+++ CSHARP/OENIK_PROG3_2019_2_J4G2NY/J4G2NY_Webshop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J4G2NY_Webshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
