0ad5ffd0351a079d919f29f589a3ec6953559654
kemenes_janos <kemenes.janos1999@gmail.com>
2019-10-20 18:10:55 +0200

    Revert "Revert "Added NuGet StyleCop, CodeAnalyzis, Entity Framework to all project""
    This reverts commit d72d9214386636fed0666e97d10ce93dcc399279.

--- CSHARP/OENIK_PROG3_2019_2_J4G2NY/Webshop.Logic.Tests/Class1.cs
+++ CSHARP/OENIK_PROG3_2019_2_J4G2NY/Webshop.Logic.Tests/Class1.cs
@@ -4,11 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Webshop.Logic
+namespace Webshop.Logic.Tests
 {
-
     public class Class1
     {
-        
     }
 }
