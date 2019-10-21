aa3ff5a47ab92edc29a91aff31ac7f4e0370d735
kemenes_janos <kemenes.janos1999@gmail.com>
2019-10-21 11:49:08 +0200

    connectionStrings copy, created menu

--- CSHARP/OENIK_PROG3_2019_2_J4G2NY/J4G2NY_Webshop/Program.cs
+++ CSHARP/OENIK_PROG3_2019_2_J4G2NY/J4G2NY_Webshop/Program.cs
@@ -10,6 +10,29 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            string v;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Mit szeretne tenni ? \n");
+                Console.WriteLine("L: kilistázni \nH: hozzáadni \nT: törölni \nM: módosítani ");
+                Console.WriteLine("U: Ugyanabban a városban lakik a vevő és az eladó");
+                Console.WriteLine("V: Városokban mennyi a vásárolt termékek átlaga");
+                Console.WriteLine("K: az egyes korosztályok melyik kategóriákra költöttek a legtöbbet");
+                Console.WriteLine("R: részesedési mutató kérése egy termékre");
+                Console.WriteLine("X: kilépni");
+                v = Console.ReadLine();
+                Console.Clear();
+                if (v == "l" || v == "h" || v == "t" || v == "m") 
+                {
+                    Console.WriteLine("Melyik táblával?\n");
+                    Console.WriteLine("U: Users \nS: Sales \nL: Loc");
+                    string t = Console.ReadLine();
+                    Console.Clear();
+                }
+                if (v != "x") { Console.WriteLine("Ez még nincs kész"); }
+                Console.ReadKey();
+            } while (v!="x");
         }
     }
 }
