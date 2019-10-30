c17c4697dd40dba9aad7d84d8730dbd01caa8579
kemenes_janos <kemenes.janos1999@gmail.com>
2019-10-30 17:04:03 +0100

    Created IRepository

---
+++ CSHARP/OENIK_PROG3_2019_2_J4G2NY/Repository/IRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    /// <summary>
+    /// CRUD methods interface.
+    /// </summary>
+    interface IRepository
+    {
+        /// <summary>
+        /// CREAT.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        void Add<T>(T obj);
+        /// <summary>
+        /// READ.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Get<T>(int id);
+        /// <summary>
+        /// UPDATE.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="oldid"></param>
+        /// <param name="newobject"></param>
+        void Update<T>(int oldid, T newobject);
+        /// <summary>
+        /// DELETE.
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(int id);
+    }
+}
