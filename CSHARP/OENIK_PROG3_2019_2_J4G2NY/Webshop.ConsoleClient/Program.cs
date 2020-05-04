974fa0898593e2aed2ee50a6fed68fdda4c6de9c
kemenes_janos <kemenes.janos1999@gmail.com>
2020-05-04 14:57:54 +0200

    Added Webshop.ConsoleClient

---
+++ CSHARP/OENIK_PROG3_2019_2_J4G2NY/Webshop.ConsoleClient/Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.ConsoleClient
+{
+    public class Location
+    {
+        
+        public decimal ID { get; set; }
+        public string Country { get; set; }
+        public string Street { get; set; }
+        public Nullable<decimal> House_Number { get; set; }
+        public Nullable<decimal> Zip_Code { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID={ID}\tCountry={Country}\tStreet={Street}\tHouse_Number={House_Number}\tZip_Code={Zip_Code}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("waiting...");
+            Console.ReadKey();
+
+            string url = "http://localhost:63263/api/LocationsApi/";
+            using(HttpClient client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url + "all").Result;
+                var list = JsonConvert.DeserializeObject<List<Location>>(json);
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.ReadKey();
+
+                Dictionary<string, string> postData;
+                string response;
+
+                postData = new Dictionary<string, string>();
+                postData.Add(nameof(Location.Country), "Magyarország");
+                postData.Add(nameof(Location.Street), "Alma Dűlő");
+                postData.Add(nameof(Location.House_Number), "12");
+                postData.Add(nameof(Location.Zip_Code), "4253");
+                postData.Add(nameof(Location.ID), "41315");
+
+                response = client.PostAsync(url + "add", new FormUrlEncodedContent
+                    (postData)).Result.Content.ReadAsStringAsync().Result;
+                json = client.GetStringAsync(url + "all").Result;
+                Console.WriteLine("ADD:" + response);
+                Console.WriteLine("All:" + json);
+                Console.ReadKey();
+
+                postData = new Dictionary<string, string>();
+                postData.Add(nameof(Location.Country), "Magyarország");
+                postData.Add(nameof(Location.Street), "Alma aholy");
+                postData.Add(nameof(Location.House_Number), "12");
+                postData.Add(nameof(Location.Zip_Code), "4253");
+                postData.Add(nameof(Location.ID), "41315");
+
+                response = client.PostAsync(url + "mod", new FormUrlEncodedContent
+                    (postData)).Result.Content.ReadAsStringAsync().Result;
+                json = client.GetStringAsync(url + "all").Result;
+                Console.WriteLine("MOD:" + response);
+                Console.WriteLine("All:" + json);
+                Console.ReadKey();
+
+                response = client.GetStringAsync(url + "del/" + "41315").Result;
+                json = client.GetStringAsync(url + "all").Result;
+                Console.WriteLine("DEL:" + response);
+                Console.WriteLine("All:" + json);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CSHARP/OENIK_PROG3_2019_2_J4G2NY/Webshop.Web/Controllers/LocationsApiController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Webshop.Logic;
+using Webshop.Repository;
+using Webshop.Web.Models;
+
+namespace Webshop.Web.Controllers
+{
+    public class LocationsApiController : ApiController
+    {
+        public class ApiResult
+        {
+            public bool OperationResult { get; set; }
+        }
+        ILogic logic;
+        IMapper mapper;
+        RepositoryHelper repoHelper;
+        public LocationsApiController()
+        {
+            this.repoHelper = new RepositoryHelper(new LocRepository(), new SaleRepository(), new UserRepository());
+            logic = new Logic.Logic(repoHelper);
+            mapper = MapperFactory.CreateMapper();
+           
+        }
+
+        // GET api/LocationsApi
+        [ActionName("all")]
+        [HttpGet]
+        public IEnumerable<Models.Location> GetAll()
+        {
+            var locations = logic.GetAllLocations();
+            return mapper.Map<IEnumerable<Data.Loc>, List<Models.Location>>(locations);
+        }
+
+        [ActionName("del")]
+        [HttpGet]
+        public ApiResult DelOneLocation(decimal id)
+        {
+            bool success = logic.DeleteLocation(id);
+            return new ApiResult() { OperationResult = success };
+        }
+
+        [ActionName("add")]
+        [HttpPost]
+        public ApiResult AddOneLocation(Location location)
+        {
+            logic.InsertLocationData(location.ID, location.Country, location.Street, (decimal)location.House_Number, (decimal)location.Zip_Code);
+            return new ApiResult() { OperationResult = true };
+        }
+
+        [ActionName("mod")]
+        [HttpPost]
+        public ApiResult ModOneLocation(Location location)
+        {
+            bool success = logic.UpdateLocation(location.ID, new Data.Loc()
+            {
+                Country = location.Country,
+                ID = location.ID,
+                Street = location.Street,
+                Zip_Code = location.Zip_Code,
+                House_Number = 20
+            });
+            return new ApiResult() { OperationResult = success };
+        }
+    }
+}
